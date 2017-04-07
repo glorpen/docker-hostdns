5dccc15e2c70dd0ff95760674fa23d845ba6786b
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2017-04-07 19:28:06 +0200

    more tests

--- src/docker_hostdns/tests.py
+++ src/docker_hostdns/tests.py
@@ -1,12 +1,13 @@
 '''
 Created on 07.04.2017
 
-@author: glorpen
+@author: Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
 '''
 import unittest.mock
-from docker_hostdns.hostdns import NamedUpdater
+from docker_hostdns.hostdns import NamedUpdater, ContainerInfo, DockerHandler
 import dns
 import contextlib
+from docker_hostdns.exceptions import ConnectionException
 
 class NamedUpdaterTest(unittest.TestCase):
     
@@ -92,3 +93,136 @@ class NamedUpdaterTest(unittest.TestCase):
             n.load_records()
         
         self.assertTrue(n.hosts.issubset([self.hostname]))
+
+class ContainerInfoTest(unittest.TestCase):
+    
+    test_id = "some-id"
+    
+    def create_container(self, id_, name, networks={}, label_name=None):
+        m = unittest.mock.Mock()
+        
+        m.attrs = {
+            "Id": id_,
+            "Name": name,
+            "NetworkSettings": {
+                "Networks": {}
+            },
+            "Config": {
+                "Labels": {}
+            }
+        }
+        
+        for name, v in networks.items():
+            m.attrs["NetworkSettings"]["Networks"][name] = {
+                "IPAddress": v[0],
+                "GlobalIPv6Address": v[1]
+            }
+        if label_name:
+            m.attrs["Config"]["Labels"]["pl.glorpen.hostname"] = label_name
+        
+        return m
+    
+    def test_getter_without_networks(self):
+        m = self.create_container(self.test_id, "/some_test__")
+        c = ContainerInfo.from_container(m)
+        self.assertEqual(c.id, self.test_id)
+        self.assertEqual(c.name, "some-test")
+        self.assertEqual(c.ipv4s, [])
+        self.assertEqual(c.ipv6s, [])
+    
+    def test_label(self):
+        m = self.create_container(self.test_id, self.test_id, label_name="name_from_label")
+        c = ContainerInfo.from_container(m)
+        self.assertEqual(c.name, "name-from-label")
+    
+    def test_networks(self):
+        m = self.create_container(
+            self.test_id,
+            self.test_id,
+            networks={"a":["ipv4.1","ipv6.1"],"b":["ipv4.2", None], "c":[None, "ipv6.2"]}
+        )
+        c = ContainerInfo.from_container(m)
+        self.assertEqual(sorted(c.ipv4s), ["ipv4.1", "ipv4.2"])
+        self.assertEqual(sorted(c.ipv6s), ["ipv6.1", "ipv6.2"])
+
+class DockerHandlerTest(unittest.TestCase):
+    def get_object(self):
+        dns_updater = unittest.mock.MagicMock()
+        d = DockerHandler(dns_updater)
+        
+        return d, dns_updater
+    
+    @contextlib.contextmanager
+    def mock_docker_client(self):
+        with unittest.mock.patch("docker.from_env") as docker:
+            client = unittest.mock.MagicMock()
+            docker.return_value = client
+            
+            yield client
+    
+    @contextlib.contextmanager
+    def mock_container_info_factory(self):
+        with unittest.mock.patch.object(ContainerInfo, 'from_container') as cinfo:
+            yield cinfo
+    
+    def test_setup_error(self):
+        d, _updater = self.get_object()
+        with self.mock_docker_client() as client:
+            client.ping.side_effect = Exception()
+            self.assertRaises(ConnectionException, d.setup)
+    
+    def test_setup(self):
+        d, updater = self.get_object()
+        with self.mock_docker_client() as client:
+            client.containers.list.return_value = [
+                ["idA", "a", ["ipv4"], ["ipv6"]],
+                ["idB", "b", ["ipv4"], []],
+                ["idC", "a", [], []],
+            ]
+            with self.mock_container_info_factory() as info:
+                info.side_effect = lambda c: ContainerInfo(id=c[0],name=c[1], ipv4s=c[2], ipv6s=c[3])
+                d.setup()
+        
+        # second "a" should be renamed to a-1
+        updater.set_hosts.assert_called_once_with({'b': (['ipv4'], []), 'a-1': ([], []), 'a': (['ipv4'], ['ipv6'])})
+    
+    def test_connection_events_handlers(self):
+        d, updater = self.get_object()
+        
+        d.on_disconnect("unknown-id")
+        updater.remove_host.assert_not_called()
+        
+        d.on_connect("known-id", "name", ["ipv4"],["ipv6"])
+        updater.add_host.assert_called_once_with("name", ["ipv4"], ["ipv6"])
+        d.on_disconnect("known-id")
+        updater.remove_host.assert_called_once_with("name")
+    
+    def test_connection_events_dispatcher(self):
+        with unittest.mock.patch.object(DockerHandler, 'on_connect') as on_connect:
+            with unittest.mock.patch.object(DockerHandler, 'on_disconnect') as on_disconnect:
+                d, updater = self.get_object()
+                
+                with self.mock_container_info_factory() as info:
+                    with self.mock_docker_client() as client:
+                        
+                        d.setup()
+                        
+                        c = ContainerInfo(id="container-1", name="c-1")
+                        info.return_value = c
+                        
+                        d.handle_event({
+                            "Type": "network",
+                            "Action": "connect",
+                            "Actor":{"Attributes":{"container":"test-id"}}
+                        })
+                        
+                        on_connect.assert_called_once_with("test-id", "c-1", None, None)
+                        
+                        d.handle_event({
+                            "Type": "network",
+                            "Action": "disconnect",
+                            "Actor":{"Attributes":{"container":"test-id"}}
+                        })
+                        
+                        on_disconnect.assert_called_once_with("test-id")
+        
